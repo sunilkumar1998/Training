f1f7ec1c81c279cecdddf1233c5c6712851a07c0
sunilkumar1998 <70696076+sunilkumar1998@users.noreply.github.com>
2021-03-26 14:19:58 +0530

    simple expression

---
+++ Day 11/SimpleExpressionTree/SimpleExpressionTree/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace SimpleExpressionTree
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("lambda expression");
+			Func<int, int> square = x => x * x;
+			Console.WriteLine(square(9));
+
+
+			Console.WriteLine("lambda statement");
+			Func<int, int> square1 = x => { return x * x; };
+			Console.WriteLine(square1(7));
+
+
+			Console.WriteLine("Simple ExpressionTree Tree");
+			System.Linq.Expressions.Expression<Func<int,int>> e= x => x* x;
+
+			var a = e.Compile();
+
+			Console.WriteLine(a(7));
+		}
+	}
+}
