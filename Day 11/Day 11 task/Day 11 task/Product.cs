ffa66b3f126e20e713eb9ec295bba3b2b46b4af6
sunilkumar1998 <70696076+sunilkumar1998@users.noreply.github.com>
2021-03-26 17:38:07 +0530

    New

---
+++ Day 11/Day 11 task/Day 11 task/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day_11_task
+{
+	class Product
+	{
+		public int Id { get; set; }
+
+		public string Title { get; set; }
+
+		public int Price { get; set; }
+
+		public string Owner { get; set; }
+	}
+}
